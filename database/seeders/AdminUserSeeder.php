460c367ded81f1b31254edde057af91df34b2f47
Sami sdk <samisdk4@gmail.com>
2025-04-21 21:31:11 +0200

    commit de sami ajout de seeder admin

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        // Check if admin already exists
+        if (!User::where('email', 'admin@example.com')->exists()) {
+            User::create([
+                'name' => 'Admin',
+                'email' => 'admin@example.com',
+                'usertype'=>'admin',
+                'password' => Hash::make('admin123'), // You can change the password
+                'email_verified_at' => now(), // Optional: if you use roles
+            ]);
+        }
+    }
+}
