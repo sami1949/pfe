8f10a9c4c07ec0013487dc511e39114eb62676ad
Sami sdk <samisdk4@gmail.com>
2025-05-17 16:42:10 +0200

    filter by categories almost done, filtering by homme and femme in navigation to handle

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -668,6 +668,7 @@ nav.scrolled .nav-scrolled\:text-black {
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                     </svg>
                     Pour Femmes
+                    
                 </div>
             </x-responsive-nav-link>
         @endauth
